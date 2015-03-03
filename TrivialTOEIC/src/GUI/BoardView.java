f67d40332bd50c24d4a6699b9dca5e81932146f8
Vincent <vincent.noyalet@telecomnancy.eu>
2015-03-03 09:10:37 +0100

    Correspondance between board and boardModel has been made

--- TrivialTOEIC/src/GUI/BoardView.java
+++ TrivialTOEIC/src/GUI/BoardView.java
@@ -13,6 +13,7 @@ import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
@@ -23,18 +24,28 @@ import javax.swing.JPanel;
 public class BoardView extends JPanel{
     
     private Board board;
+    private HashMap<Integer,int[]> correspondanceHm;
     private BufferedImage imageCasual;
     private BufferedImage  imageCenter;
     private BufferedImage  imageCross;
     private BufferedImage  imageBase;
+    private BufferedImage  imageCategory1;
+    private BufferedImage  imageCategory2;
+    private BufferedImage  imageCategory3;
+    private BufferedImage  imageCategory4;
 
     BoardView(Game currentGame) {
         this.setPreferredSize(new Dimension(500,500));
         this.board=currentGame.getBoard();
+        this.correspondanceHm = this.board.getCorrHashmap();
         this.setBackground(Color.LIGHT_GRAY);
         
         try {                
           imageCasual = ImageIO.read(new File("ressources/github.png"));
+          imageCategory1 = ImageIO.read(new File("ressources/Category1.png"));
+          imageCategory2 = ImageIO.read(new File("ressources/Category2.png"));
+          imageCategory3 = ImageIO.read(new File("ressources/Category3.png"));
+          imageCategory4 = ImageIO.read(new File("ressources/Category4.png"));
           imageCenter = ImageIO.read(new File("ressources/center.png"));
           imageCross = ImageIO.read(new File("ressources/cross.png"));
           imageBase = ImageIO.read(new File("ressources/base.png"));
@@ -64,11 +75,31 @@ public class BoardView extends JPanel{
                 
                 // casual
                 else if(i%4==0||j%4==0){
-                    g.drawImage(imageCasual, i*50+10, j*50+10, this);
+                    int[] tempArray={i,j};
+                    int index = this.board.getIndexFromVal(tempArray);
+                    String type = this.board.getCategory(index);
+                    switch(type){
+                        case "Category 1":
+                            g.drawImage(imageCategory1, i*50+10, j*50+10, this);
+                            break;
+                            
+                        case "Category 2":
+                            g.drawImage(imageCategory2, i*50+10, j*50+10, this);
+                            break;
+                        
+                        case "Category 3":
+                            g.drawImage(imageCategory3, i*50+10, j*50+10, this);
+                            break;
+                            
+                        case "Category 4":
+                            g.drawImage(imageCategory4, i*50+10, j*50+10, this);
+                            break;
+                         
+                        default:
+                            System.out.println("Error"); 
+                    }
                 }
-                
             }
         }
     }
-    
 }
