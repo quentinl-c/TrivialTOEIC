9f323ae692555fbdf6dc9c8aba432e9c6b13eebf
Quentin Laporte-Chabasse <quentin.laporte-chabasse@telecomnancy.net>
2015-03-21 10:17:29 +0100

    Correct some display bugs

--- TrivialTOEIC/src/GUI/PlayerListPanel.java
+++ TrivialTOEIC/src/GUI/PlayerListPanel.java
@@ -97,13 +97,13 @@ public class PlayerListPanel extends Panel {
     
     public void playerButtonsEnable(boolean bool){
         this.addPlayer.setEnabled(bool);
-        //this.addPlayer.setVisible(bool);
+        this.addPlayer.setVisible(bool);
         
         this.nameField.setEnabled(bool);
-        //this.nameField.setVisible(bool);
+        this.nameField.setVisible(bool);
         
         this.delPlayer.setEnabled(bool);
-        //this.delPlayer.setVisible(bool);
+        this.delPlayer.setVisible(bool);
 
     }
 }
